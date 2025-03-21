72ad35f68176675a3ebab7d2e22eed9c0bf068da
emmanuel <emmanjeru2002@gmail.com>
2025-03-21 16:07:29 +0300

    final commit

--- lib/testing/article_test.py
+++ lib/testing/article_test.py
@@ -1,8 +1,5 @@
 import pytest
 
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'path_to_classes_directory')))
 from classes.many_to_many import Article
 from classes.many_to_many import Magazine
 from classes.many_to_many import Author
@@ -26,21 +23,16 @@ class TestArticle:
         author = Author("Carry Bradshaw")
         magazine = Magazine("Vogue", "Fashion")
         article_1 = Article(author, magazine, "How to wear a tutu with style")
-    
-        # Ensure that setting the title after initialization raises an AttributeError
-        with self.assertRaises(AttributeError):
-            article_1.title = 500
-
 
         # comment out the next two lines if using Exceptions
-        article_1.title = 500
-        assert article_1.title == "How to wear a tutu with style"
+        # article_1.title = 500
+        # assert article_1.title == "How to wear a tutu with style"
         
         assert isinstance(article_1.title, str)
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Article(author, magazine, 500)
+        with pytest.raises(Exception):
+            Article(author, magazine, 500)
 
     def test_title_is_valid(self):
         """title is between 5 and 50 characters inclusive"""
@@ -51,12 +43,12 @@ class TestArticle:
         assert 5 <= len(article_1.title) <= 50
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Article(author, magazine, "Test")
+        with pytest.raises(Exception):
+            Article(author, magazine, "Test")
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Article(author, magazine, "How to wear a tutu with style and walk confidently down the street")
+        with pytest.raises(Exception):
+            Article(author, magazine, "How to wear a tutu with style and walk confidently down the street")
 
     def test_has_an_author(self):
         """article has an author"""
